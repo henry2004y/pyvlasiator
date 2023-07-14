2414e7210c1ac2a2bdf1e18788f7f3d2d21bc27f
Hongyang Zhou <hyzhou@umich.edu>
2023-07-15 00:54:04 +0800

    Tweak package structure

--- src/vlasiator/vlasiator.py
+++
@@ -1,9 +0,0 @@
-import vlsvvariables
-from vlsvvariables import data_operators
-
-import vlsvreader
-from vlsvreader import VlsvReader
-
-
-if __name__ == "__main__":
-    print("Running from CLI...")
